fd71aca2fa715e8ce171ef06d2a5940f9a476bb9
Rahiel Kasim <rahiel@protonmail.ch>
2016-01-03 23:39:11 +0100

    let pypi update script exit if something is wrong

--- update-pypi.bash
+++ update-pypi.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-rm -R dist/
+set -e
+python3 -c 'import pypandoc'
+
+rm -rf dist/
 python3 setup.py sdist           # source distribution
 python3 setup.py bdist_wheel     # built package
 # gpg --detach-sign -a dist/*
